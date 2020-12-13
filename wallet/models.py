dd69919aacc0172eac5102b4ad4a6862f613e119
MohmmadReza-Jafari <chashm78@gmail.com>
2020-12-13 17:35:52 +0330

    create wallet model

--- wallet/models.py
+++ wallet/models.py
@@ -8,3 +8,4 @@ class Wallet(models.Model):
 
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     amount = models.FloatField(default=0.0)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='wallet')
